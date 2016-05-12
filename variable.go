3d33ab9dc028565c73c4740516179b30e864df9e
honey <honeym90@gmail.com>
2016-05-12 17:47:23 +0530

    variables in Golang

---
+++ variable.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	a := 10
+	b := "golang"
+	c := 4.5
+	d := false
+	fmt.Printf("%v \n",a)
+	fmt.Printf("%v \n",b)
+	fmt.Printf("%v \n",c)
+	fmt.Printf("%v \n",d)
+}
+
